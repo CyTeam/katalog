929d36596d8b21c2c6463db54c23f7858100e25c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-10 14:08:17 +0200

    Rename application to Katalog.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Dossier::Application.configure do
+Katalog::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Dossier::Application.routes.draw do |map|
+Katalog::Application.routes.draw do |map|
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
