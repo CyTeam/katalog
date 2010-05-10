3eed5b0ac02995375d722c4bea44918ba4f0f2f7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-10 14:08:17 +0200

    Rename application to Katalog.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Dossier::Application.configure do
+Katalog::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
