7afd2979dea978eef07112c6b060e21de01d05f0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-11 23:56:29 +0200

    Implement simple CSV importer.

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -1,2 +1,23 @@
 class Dossier < ActiveRecord::Base
+  # Importer
+  def self.import_from_csv(path)
+    # Load file at path using ; as delimiter
+    rows = FasterCSV.read(path, :col_sep => ';')
+    
+    # Select rows containing main dossier records by simply testing on two columns in first row
+    dossier_rows = rows.select{|row| row[0] && row[0].split('.').count == 3}
+
+    transaction do
+      for row in dossier_rows
+        dossier = self.new(
+          :signature => row[0],
+          :title     => row[1],
+          :kind      => row[9],
+          :location  => row[10]
+        )
+        
+        dossier.save!
+      end
+    end
+  end
 end
--- test/unit/dossier_test.rb
+++ test/unit/dossier_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class DossierTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  # Import
+  test "small import" do
+    rows = Dossier.import_from_csv('test/fixtures/import/small.csv')
+    assert_equal 48, Dossier.count
   end
 end
