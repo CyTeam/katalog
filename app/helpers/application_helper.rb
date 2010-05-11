01b138a04ccf645a9397209eb68dcc2b6a35d07a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-11 22:40:21 +0200

    Implement navigation_item helper for left navigation items.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def navigation_item(title, *args)
+    if current_page?(*args)
+      link = tag("span", :class => "active") + title.html_safe + "</span>".html_safe
+      item = tag("li", :class => "active") + link + "</li>".html_safe
+    else
+      item = tag("li") + link_to(title, *args) + "</li>".html_safe
+    end
+
+    return item
+  end
 end
