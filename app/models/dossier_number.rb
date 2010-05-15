690b3f68bd64433c46bd7d0d73e9308b27e15740
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-15 23:00:06 +0200

    Add {from,to}_year and period attribute helpers to DossierNumber; add unit tests.

--- app/models/dossier_number.rb
+++ app/models/dossier_number.rb
@@ -1,3 +1,35 @@
 class DossierNumber < ActiveRecord::Base
   belongs_to :dossier
+
+  # Attributes
+  def from_year
+    from.year
+  end
+  
+  def from_year=(value)
+    self.from = Date.new(value.to_i, 1, 1)
+  end
+  
+  def to_year
+    to.year
+  end
+  
+  def to_year=(value)
+    self.to = Date.new(value.to_i, 12, 31)
+  end
+  
+  def period
+    [from_year, to_year].compact.join(' - ')
+  end
+  
+  def period=(value)
+    # Convert integers to string
+    value = value.to_s
+    
+    if value =~ /-/
+      self.from_year, self.to_year = value.split('-').map{|year| year.strip }
+    else
+      self.from_year = self.to_year = value.strip
+    end
+  end
 end
