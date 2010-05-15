5d04450fbbf44af57cc697dd9f0e2279393c8f54
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-15 21:11:50 +0200

    Use dossiers index as root.

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Katalog::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "dossiers#index"
 
   # See how all your routes lay out with "rake routes"
 
