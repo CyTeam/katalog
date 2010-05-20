e08b3fb1d26cb86c5d2f12a0f87bdaeaac8a55d8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-20 21:34:20 +0200

    Use content_tag instead of tag.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
   def navigation_item(title, *args)
     if current_page?(*args)
-      link = tag("span", :class => "active") + title.html_safe + "</span>".html_safe
-      item = tag("li", :class => "active") + link + "</li>".html_safe
+      content_tag("li", :class => "active") do
+        content_tag("span", title, :class => "active")
+      end
     else
-      item = tag("li") + link_to(title, *args) + "</li>".html_safe
+      content_tag("li") do
+        link_to(title, *args)
+      end
     end
-
-    return item
   end
 end
