eeec1e681541dcfafac92a28e5ea62da213dcfaa
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-06-14 21:36:12 +0200

    Add seeding for some locations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Location.create([
+  {:code => "EG", :title => "Erdgeschoss St. Oswaldsgasse 16", :address => "St. Oswaldsgasse 16", :availability => "Diese Dokumente sind bei uns sofort einsehbar."},
+  {:code => "LE", :title => "Lesesal St. Oswaldsgasse 16", :address => "St. Oswaldsgasse 16", :availability => "Diese Dokumente sind bei uns sofort einsehbar."},
+  {:code => "RI", :title => "Rigistrasse", :address => "Rigistrasse", :availability => "Wünschen Sie ein Dokument von hier, beachten Sie bitte die Wartezeit von einem Tag."},
+  {:code => "CO", :title => "Rollgestell St. Oswaldsgasse 16", :address => "St. Oswaldsgasse 16", :availability => "Diese Dokumente sind bei uns sofort einsehbar."}
+])
