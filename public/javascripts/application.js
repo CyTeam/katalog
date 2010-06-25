a45f73fc21fb1d0704121a7a0e8f7ff78917880a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-06-25 10:22:26 +0200

    Support data-href attribute on <tr> to make whole line a link.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+document.observe("dom:loaded", function() {
+  elements = $$("tr[data-href]");
+  elements.each(function(element) {
+    element.style.cursor = "pointer";
+  });
+  
+  $(document.body).observe("click", function(event) {
+    var link = event.findElement("a");
+    if (link) {
+      return true;
+    };
+    
+    var element = event.findElement("tr[data-href]");
+    if (element) {
+      var href = element.readAttribute('data-href');
+      document.location.href=href;
+
+      event.stop();
+      return false;
+    }
+  });
+});
