eb9258b131ed14ecd904da3da5e73f703bbcc6e4
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-05 08:36:44 +0200

    Copy i18n helpers from CyDoc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,28 @@
 module ApplicationHelper
+  # i18n
+  def t_attr(attribute, model = nil)
+    if model.is_a? Class
+      model_name = model.name.underscore
+    elsif model.nil?
+      model_name = controller_name.singularize
+    end
+    t(attribute, :scope => [:activerecord, :attributes, model_name])
+  end
+
+  def t_model(model = nil)
+    if model.is_a? Class
+      model_name = model.name.underscore
+    elsif model.nil?
+      model_name = controller_name.singularize
+    end
+    t(model_name, :scope => [:activerecord, :models])
+  end
+
+  def t_confirm_delete(record)
+    t('messages.confirm_delete', :record => "#{t_model(record.class)} #{record.to_s}")
+  end
+
+  # Navigation
   def navigation_item(title, *args)
     if current_page?(*args)
       content_tag("li", :class => "active") do
