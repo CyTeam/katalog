ce5e5acb23376d6e13db8dacaa541b72ee2f7106
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-06 08:51:55 +0200

    Test containers association on dossier model.

--- test/unit/dossier_test.rb
+++ test/unit/dossier_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class DossierTest < ActiveSupport::TestCase
-  # Import
-  setup do
+  test "container association" do
+    assert dossiers(:city_counsil).containers.include?(containers(:city_counsil))
+
+    assert dossiers(:city_history).containers.include?(containers(:city_history_1900_1999))
+    assert_equal 3, dossiers(:city_history).containers.count
   end
 end
