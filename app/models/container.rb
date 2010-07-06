0b9b3e4e0e9b4e319f75646e31f6b58c8178b669
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-06 10:29:30 +0200

    Enhance and fix location and container_type assignment in container model.

--- app/models/container.rb
+++ app/models/container.rb
@@ -14,4 +14,24 @@ class Container < ActiveRecord::Base
   def title
     dossier.title
   end
+
+  def container_type=(value)
+    value = ContainerType.find_by_code(value) if value.is_a?(String)
+    self[:container_type_id] = value.id
+    self.container_type.reload
+  end
+  
+  def location=(value)
+    value = Location.find_by_code(value) if value.is_a?(String)
+    self[:location_id] = value.id
+    self.location.reload
+  end
+  
+  # Import
+  def import(row)
+    container = self.create(
+      :document_type => row[9],
+      :location      => row[10]
+    )
+  end
 end
--- test/unit/container_test.rb
+++ test/unit/container_test.rb
@@ -12,4 +12,34 @@ class ContainerTest < ActiveSupport::TestCase
   test "to_s" do
     assert_equal "City counsil (DH@EG)", containers(:city_counsil).to_s
   end
+
+  test "location assignment" do
+    container = Container.new
+    
+    assert_nil container.location
+
+    container.location = "RI"
+    assert_equal locations(:RI), container.location
+    
+    container.location = "EG"
+    assert_equal locations(:EG), container.location
+    
+    container.location = locations(:RI)
+    assert_equal locations(:RI), container.location
+  end
+
+  test "container type assignment" do
+    container = Container.new
+    
+    assert_nil container.container_type
+
+    container.container_type = "DA"
+    assert_equal container_types(:DA), container.container_type
+    
+    container.container_type = "DH"
+    assert_equal container_types(:DH), container.container_type
+    
+    container.container_type = container_types(:DA)
+    assert_equal container_types(:DA), container.container_type
+  end
 end
