e4a10b54afe176940a45ba03b3a0edddd8017641
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-06 08:42:08 +0200

    Test dossier association on container test.

--- test/unit/container_test.rb
+++ test/unit/container_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class ContainerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "dossier association" do
+    assert_equal dossiers(:city_counsil), containers(:city_counsil).dossier
   end
 end
