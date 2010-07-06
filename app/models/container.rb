aed576cdfed14c561c7d22b13ee3c7319cd69b15
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-06 09:13:38 +0200

    Implement and test Container.to_s.

--- app/models/container.rb
+++ app/models/container.rb
@@ -5,4 +5,13 @@ class Container < ActiveRecord::Base
   belongs_to :container_type
   has_many :numbers, :class_name => 'DossierNumber'
   accepts_nested_attributes_for :numbers
+
+  # Helpers
+  def to_s
+    "#{title} (#{container_type.code}@#{location.code})"
+  end
+
+  def title
+    dossier.title
+  end
 end
--- test/unit/container_test.rb
+++ test/unit/container_test.rb
@@ -4,4 +4,12 @@ class ContainerTest < ActiveSupport::TestCase
   test "dossier association" do
     assert_equal dossiers(:city_counsil), containers(:city_counsil).dossier
   end
+  
+  test "title" do
+    assert_equal containers(:city_counsil).dossier.title, containers(:city_counsil).title
+  end
+  
+  test "to_s" do
+    assert_equal "City counsil (DH@EG)", containers(:city_counsil).to_s
+  end
 end
