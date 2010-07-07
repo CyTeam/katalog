c45741002be0635f458fd533d0f8b68c200895f7
Ramon Egloff <Ramon Egloff (CyT) ramon.egloff@cyt.ch>
2010-07-07 10:28:46 +0200

    Implement crud translation helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,17 @@ module ApplicationHelper
     t(model_name, :scope => [:activerecord, :models])
   end
 
+  def t_crud(action = nil, model = nil)
+    if model.is_a? Class
+      model_name = model.name.underscore
+    elsif model.nil?
+      model_name = controller_name.singularize
+    end
+    
+    action ||= action_name
+    t(action, :scope => :crud, :model => model_name)
+  end
+  
   def t_confirm_delete(record)
     t('messages.confirm_delete', :record => "#{t_model(record.class)} #{record.to_s}")
   end
