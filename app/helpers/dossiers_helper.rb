311cfcde07c69570a60581c43b2c99d4caf96dd5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-28 09:07:43 +0200

    Linkify topic items in list.

--- app/helpers/dossiers_helper.rb
+++ app/helpers/dossiers_helper.rb
@@ -2,4 +2,12 @@ module DossiersHelper
   def link_to_keyword(keyword)
     link_to keyword, dossiers_path(:dossier => {:tag => keyword })
   end
+
+  def url_for_topic(topic)
+    dossiers_url(:dossier => {:signature => topic.signature})
+  end
+  
+  def link_to_topic(topic)
+    link_to(topic, dossiers_url(topic))
+  end
 end
