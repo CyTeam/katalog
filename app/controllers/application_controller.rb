540719d86a46f74bf4e8f5602c8dd06fdc6f3edf
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-01 13:55:04 +0200

    Move inlined in_place_edit_for controller method to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
+  def self.in_place_edit_for(object, attribute, options = {})
+    define_method("set_#{attribute}") do
+      unless [:post, :put].include?(request.method_symbol) then
+        return render(:text => 'Method not exactly allowed', :status => 405)
+      end
+      @item = object.to_s.camelize.constantize.find(params[:id])
+      @item.update_attribute(attribute, params[:value])
+      render :text => CGI::escapeHTML(@item.send(attribute).to_s)
+    end
+  end
+
 end
--- app/controllers/dossier_numbers_controller.rb
+++ app/controllers/dossier_numbers_controller.rb
@@ -1,14 +1,4 @@
 class DossierNumbersController < ApplicationController
-  def self.in_place_edit_for(object, attribute, options = {})
-    define_method("set_#{attribute}") do
-      unless [:post, :put].include?(request.method_symbol) then
-        return render(:text => 'Method not exactly allowed', :status => 405)
-      end
-      @item = object.to_s.camelize.constantize.find(params[:id])
-      @item.update_attribute(attribute, params[:value])
-      render :text => CGI::escapeHTML(@item.send(attribute).to_s)
-    end
-  end
 
   in_place_edit_for :dossier_number, :amount
 end
