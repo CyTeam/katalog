13cdfdac721be695ffcf14b00e9b5ec3bc825539
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-09 11:19:45 +0200

    Add translation and style config for will_paginate into initializer.

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,3 @@
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Vorherige Seite'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Nächste Seite'
+WillPaginate::ViewHelpers.pagination_options[:class] = 'digg_pagination'
