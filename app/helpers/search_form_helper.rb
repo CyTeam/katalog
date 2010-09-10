9924f39987b43dc447d37b5dd2df2e1832940a5f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-10 14:06:29 +0200

    Smaller heading search; dropped sidebar search form.

--- app/helpers/search_form_helper.rb
+++ app/helpers/search_form_helper.rb
@@ -2,6 +2,6 @@ module SearchFormHelper
   def search_form_for(object_name, options={}, &block)
     options[:html] = {:method => :get}.update(options[:html] || {})
     object = OpenStruct.new(params[object_name])
-    semantic_form_for(object_name, object, options, &block)
+    form_for(object_name, object, options, &block)
   end
 end
