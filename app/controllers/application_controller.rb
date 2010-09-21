d58c30666b23b677a1093e40ae8f2c3cbaa77c32
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-21 10:08:47 +0200

    Ugly monkey patching acts_as_taggable to trigger reindexing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,3 +14,28 @@ class ApplicationController < ActionController::Base
   end
 
 end
+
+# Ugly monkey patching acts_as_taggable to trigger reindexing
+ActsAsTaggableOn::Tag
+class ActsAsTaggableOn::Tag
+  after_save :touch_taggings
+  after_destroy :touch_taggings
+  
+  private
+  def touch_taggings
+    taggings.map{|t| t.save}
+  end
+end
+
+ActsAsTaggableOn::Tagging
+class ActsAsTaggableOn::Tagging
+  after_save :set_dossier_delta_flag
+  
+  private
+  def set_dossier_delta_flag
+    if taggable.is_a? Dossier
+      taggable.delta = true
+      taggable.save
+    end
+  end
+end
