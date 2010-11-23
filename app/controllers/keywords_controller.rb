1060b7498c479d0e02f114ceb9e6a9e9a179e12b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-23 11:20:35 +0100

    Use keywords controller for index.

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,6 +1,6 @@
 class KeywordsController < InheritedResources::Base
   # Association
-  belongs_to :dossier
+  optional_belongs_to :dossier
   
   # Authentication
   before_filter :authenticate_user!, :except => [:index, :search, :show]
@@ -9,6 +9,10 @@ class KeywordsController < InheritedResources::Base
   respond_to :html, :js
 
   # Actions
+  def index
+    @keywords = Dossier.keyword_counts.order(:name).paginate(:per_page => params[:per_page], :page => params[:page])
+  end
+  
   def create
     @dossier = Dossier.find(params[:dossier_id])
     @keywords = @dossier.keyword_list.add(params[:keyword][:name])
