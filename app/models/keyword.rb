8ddbdbb47ec595ad459f789b0be4b7d32237a433
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-24 14:38:05 +0100

    Add default scope to Keyword actually restricting rectords to be keywords.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,3 +1,4 @@
 class Keyword < ActsAsTaggableOn::Tag
   scope :by_character, lambda {|value| where("name LIKE CONCAT(?, '%')", value)}
+  default_scope includes(:taggings).where("taggings.context = 'keywords'")
 end
