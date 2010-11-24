eb08ea88d04b35e88e544affd4f8f32db808731b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-24 14:45:41 +0100

    Better param update code in link renderer.

--- lib/pagination_list_link_renderer.rb
+++ lib/pagination_list_link_renderer.rb
@@ -1,6 +1,9 @@
 class PaginationListLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
 
   def html_container(html)
+    @search_key = :search
+    @params = @template.params[@search_key] || {}
+    
     html += "<span>|</span>" + [per_page_link(25), per_page_link(50), per_page_link(200)].join('')
     html += "</div><div style='clear: both; padding-top: 0.5em;'>" + alphabetic_pagination
     html = "<div style='clear: both'>" + html + "</div>"
@@ -14,7 +17,7 @@ class PaginationListLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
   end
 
   def alphabetic_page_link(character)
-    if @template.params[:search] && character == @template.params[:search][:by_character]
+    if character == @params[:by_character]
       link = "<em>%s</em>" % character.upcase
     else
       link = "<a class='per_page' href='%s'>%s</a>" % [alphabetic_page_href(character), character.upcase]
@@ -24,8 +27,8 @@ class PaginationListLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
   end
   
   def alphabetic_page_href(character)
-    search_params = (@template.params[:search] || {}).merge(:by_character => character)
-    params = @template.params.merge(:search => search_params)
+    search_params = @params.merge(:by_character => character)
+    params = @template.params.merge(@search_key => search_params)
     
     @template.url_for(params)
   end
