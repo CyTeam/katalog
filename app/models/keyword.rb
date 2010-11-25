2654362df4ab41ee0efe147d1998276589948641
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-25 11:20:31 +0100

    Better scoping to restrict Keyword to keywords tags.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,8 +1,8 @@
 class Keyword < ActsAsTaggableOn::Tag
   scope :by_character, lambda {|value| where("name LIKE CONCAT(?, '%')", value)}
   scope :characters, select("DISTINCT substring(upper(name), 1, 1) AS letter").having("letter BETWEEN 'A' AND 'Z'")
-  default_scope joins(:taggings).where("taggings.context = 'keywords'")
-
+  default_scope where("id IN (SELECT tag_id FROM taggings WHERE taggings.context = 'keywords')")
+  
   def self.character_list
     characters.map{|t| t.letter}
   end
