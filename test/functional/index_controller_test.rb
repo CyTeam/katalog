97f95e11e5aae9af5f00519c77de82cd9d154e68
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-30 11:25:38 +0100

    Move keyword functional tests from index controller to keywords.

--- test/functional/index_controller_test.rb
+++ test/functional/index_controller_test.rb
@@ -3,13 +3,6 @@ require 'test_helper'
 class IndexControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   
-  test "should get keyword" do
-    get :keyword
-    assert_response :success
-
-    assert_not_nil assigns(:index)
-  end
-
   test "should get title" do
     get :title
     assert_response :success
--- test/functional/keywords_controller_test.rb
+++ test/functional/keywords_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class KeywordsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  include Devise::TestHelpers
+  
+  test "should get keyword index" do
+    get :index
+    assert_response :success
+
+    assert_not_nil assigns(:keywords)
+    assert_equal assigns(:paginated_scope), Keyword
+  end
+
+  test "should get keyword search" do
+    get :search, :search => {:text => 'test'}
+    assert_response :success
+
+    assert_equal "test", assigns(:query)
+    assert_not_nil assigns(:keywords)
+    assert_equal Keyword.where("name LIKE ?", "%test%"), assigns(:paginated_scope)
   end
 end
