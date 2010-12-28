5699492a1a9d5d9ff7dfd7558fa21ce1cb046d69
Roman Simecek <roman.simecek@cyt.ch>
2010-12-28 12:43:11 +0100

    Monkey patched i18n rails helpers so that the links have a title attribute.

---
+++ config/initializers/i18n_rails_helpers.rb
@@ -0,0 +1,9 @@
+require 'i18n_rails_helpers'
+
+ContextualLinkHelpers.class_eval do
+  def icon_link_to(action, url, options = {})
+    options.merge!(:class => "icon icon-#{action}", :title => t("tooltips.contextual.#{action}"))
+
+    link_to(t_action(action), url, options)
+  end
+end
