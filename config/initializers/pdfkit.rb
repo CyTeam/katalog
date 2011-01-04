69003a078f815c5029a3d3e25744d4aa22a7ad3e
Roman Simecek <roman.simecek@cyt.ch>
2011-01-04 10:53:02 +0100

    Unnessesary configuration for pdfkit removed.

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/home/roman/.rvm/gems/ruby-1.8.7-p302@katalog/bin/wkhtmltopdf'
+  #config.wkhtmltopdf = ''
   config.default_options = {
     :page_size => 'A4',
     :print_media_type => true
