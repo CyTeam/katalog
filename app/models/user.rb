be50fb1a105b39d740bf8b99d13320ae888bc07e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-01-28 09:26:25 +0100

    Add User methods for authorization role aspect.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,29 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
 
+  # Authorization roles
   has_and_belongs_to_many :roles, :autosave => true
+  scope :by_role, lambda{|role| include(:roles).where(:name => role)}
+  attr_accessible :role_texts
 
+  # Return current role name.
+  def role_name
+    # Just return _a_ role, no preferences or guarantees...
+    roles.last.name
+  end
+  
   def role?(role)
     !!self.roles.find_by_name(role.to_s)
+  end
+
+  def role_texts
+    roles.map{|role| role.name}
+  end
+  
+  def role_texts=(role_names)
+    roles.delete_all
+    for role_name in role_names
+      roles.build(:name => role_name)
+    end
   end
 end
