1e767e2649bd5b27c673b2b150888c14c8121a5f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-02-25 14:44:11 +0100

    Add attribute lists to location and container_type controllers; use inherited views for those types.

--- app/controllers/container_types_controller.rb
+++ app/controllers/container_types_controller.rb
@@ -1,4 +1,8 @@
 class ContainerTypesController < AuthorizedController
   # Authentication
   before_filter :authenticate_user!, :except => [:index, :show]
+  
+  def attributes
+    ['title', 'code', 'description']
+  end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,8 @@
 class LocationsController < AuthorizedController
   # Authentication
   before_filter :authenticate_user!, :except => [:index, :show]
+
+  def attributes
+    ['title', 'code', 'address', 'availability']
+  end
 end
