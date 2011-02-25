4a62b861d4235c013a019ec758248956ced48cf9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-02-25 13:42:41 +0100

    Drop unneeded collection setup in locations and container_types controllers.

--- app/controllers/container_types_controller.rb
+++ app/controllers/container_types_controller.rb
@@ -1,9 +1,4 @@
 class ContainerTypesController < AuthorizedController
   # Authentication
   before_filter :authenticate_user!, :except => [:index, :show]
-
-  protected
-  def collection
-    @container_types ||= end_of_association_chain.paginate(:page => params[:page])
-  end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,4 @@
 class LocationsController < AuthorizedController
   # Authentication
   before_filter :authenticate_user!, :except => [:index, :show]
-
-  protected
-  def collection
-    @locations ||= end_of_association_chain.paginate(:page => params[:page])
-  end
 end
