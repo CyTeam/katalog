0c01103418d46c5a4a75ca269e235a4e3c61b67c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-01 12:09:42 +0100

    Signature comes before title in column definitions.

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -5,7 +5,7 @@ module ReportsHelper
   end
   
   def report_columns_for_select
-    columns = [:title, :signature, :first_document_year, :keyword_text, :container_type, :location, :document_count, :keywords]
+    columns = [:signature, :title, :first_document_year, :keyword_text, :container_type, :location, :document_count, :keywords]
     
     columns.map{|column| [t_attr(column, Dossier), column]}
   end
