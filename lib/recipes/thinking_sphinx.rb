cad146cb342dd45ab4cc9d7261686906970510d3
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-04 10:01:13 +0100

    Generate sphinx config from database.yml.

--- lib/recipes/thinking_sphinx.rb
+++ lib/recipes/thinking_sphinx.rb
@@ -5,7 +5,23 @@ namespace :ts do
   desc "Create thinking sphinx config"
   task :setup do
     run "mkdir -p #{shared_path}/config"
-    upload "config/development.sphinx.conf.example", "#{shared_path}/config/#{rails_env}.sphinx.conf", :via => :scp
+    
+    # Load parameters
+    db_yml = capture "cat #{deploy_to}/current/config/database.yml"
+    yaml = YAML::load(db_yml)
+    db_conf = yaml[rails_env]
+    
+    db_host     = db_conf['host']
+    db_user     = db_conf['username']
+    db_database = db_conf['database']
+    db_socket   = db_conf['socket']
+    db_password = db_conf['password']
+    
+    # Load template
+    template = File.read("config/sphinx.conf.example.erb")
+    config = ERB.new(template)
+    # Write config file
+    put config.result(binding), "#{shared_path}/config/#{rails_env}.sphinx.conf"
 
     run "mkdir -p #{shared_path}/config/sphinx"
   end
