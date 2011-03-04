e669ab35a370a1e56a1cc110372e978003ba7d92
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-04 09:30:25 +0100

    mysql:restore cap task now works; added some logging.

--- lib/recipes/mysql.rb
+++ lib/recipes/mysql.rb
@@ -1,3 +1,9 @@
+# MySQL Backup/Restore tasks
+#
+# Based on code from:
+# * http://snippets.aktagon.com/snippets/20-Capistrano-2-task-for-backing-up-your-MySQL-production-database-before-each-deployment
+# * http://www.martin-probst.com/blog/2007/11/20/mysql-backup-restore-task-for-capistrano/
+
 namespace :mysql do
   task :backup, :roles => :db, :only => { :primary => true } do
     backup_dir ||= "#{deploy_to}/backups"
@@ -8,6 +14,8 @@ namespace :mysql do
     yaml = YAML::load(text)
 
     on_rollback { run "rm #{filename}" }
+
+    logger.info "Backing up to #{filename}..."
     run "mysqldump -u #{yaml[rails_env]['username']} -p #{yaml[rails_env]['database']} -h #{yaml[rails_env]['host']}| bzip2 -c > #{filename}" do |ch, stream, out|
       if out =~ /^Enter password:/
         ch.send_data "#{yaml[rails_env]['password']}\n"
@@ -17,6 +25,8 @@ namespace :mysql do
     end
     
     run "ln -nfs #{filename} #{backup_dir}/#{application}.latest"
+    
+    logger.info "Backup successfull."
   end
 
   task :restore, :roles => :db, :only => { :primary => true } do
@@ -26,12 +36,11 @@ namespace :mysql do
     text = capture "cat #{deploy_to}/current/config/database.yml"
     yaml = YAML::load(text)
 
-    run "bzip2 -d < #{filename} | mysql -u #{yaml[rails_env]['username']} -p=\"#{yaml[rails_env]['password']}\" #{yaml[rails_env]['database']} -h #{yaml[rails_env]['host']}" do |ch, stream, out|
-      if out =~ /^Enter password:/
-        ch.send_data "#{yaml[rails_env]['password']}"
-      else
-        puts out
-      end
+    logger.info "Restoring from #{filename}..."
+    run "bzip2 -d < #{filename} |mysql -u #{yaml[rails_env]['username']} -p#{yaml[rails_env]['password']} -h #{yaml[rails_env]['host']} #{yaml[rails_env]['database']}" do |ch, stream, out|
+      puts out
     end
+    
+    logger.info "Restore successfull."
   end
 end
