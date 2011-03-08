6b84f43336a5c1a59000e717b62453746cb6257f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-08 09:54:25 +0100

    Add task descriptions for import tasks.

--- lib/recipes/import.rb
+++ lib/recipes/import.rb
@@ -19,6 +19,7 @@ namespace :import do
     run "xls2csv -d utf-8 -c';' #{shared_path}/data/dossiers.xls > #{shared_path}/data/dossiers.csv"
   end  
 
+  desc "Clone current environment database as import database."
   task :seed_import_db, :roles => :import do
     old_rails_env = rails_env
     mysql.backup
@@ -29,6 +30,7 @@ namespace :import do
     set :rails_env, old_rails_env
   end
 
+  desc "Clone import database to current environment."
   task :push_import_db, :roles => :import do
     old_rails_env = rails_env
     set :rails_env, 'import'
@@ -38,6 +40,7 @@ namespace :import do
     mysql.restore
   end
   
+  desc "Import CSV file."
   task :import_csv, :roles => :import do
     old_rails_env = rails_env
     set :rails_env, 'import'
@@ -46,6 +49,7 @@ namespace :import do
     set :rails_env, old_rails_env
   end
   
+  desc "Do a full import."
   task :default do
     push
     seed_import_db
