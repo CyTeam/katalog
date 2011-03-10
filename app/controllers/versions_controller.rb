796b5150eb30785f6c9864d04675fa7c6c18d050
Roman Simecek <roman.simecek@cyt.ch>
2011-03-10 12:10:20 +0100

    Paper trail modifications who are not working.

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -27,6 +27,17 @@ class VersionsController < AuthorizedController
       else
         object.save
       end
+      object.id_attributes.each do |name, id|
+        sub_version = Version.find_by_item_id(id)
+        sub_object = sub_version.reify
+        sub_original = ('destroy'.eql?sub_version.event ? nil : sub_version.item_type.constantize.find(sub_version.item_id))
+        if sub_original
+          sub_original = sub_object
+          sub_original.save
+        else
+          sub_object.save
+        end
+      end
     else
       version.item_type.constantize.find(version.item_id).destroy
     end
--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -3,3 +3,9 @@ require 'paper_trail/version'
 Version.class_eval do
   default_scope :order => 'created_at DESC'
 end
+
+ActiveRecord::Base.class_eval do
+  def id_attributes
+    attributes.select {|a, b| a.end_with?'id' and not a.eql?'id' }
+  end
+end
