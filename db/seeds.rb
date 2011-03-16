5289e84f717fac493f0669bb14d990dae034f446
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-16 07:46:26 +0100

    Add default reports to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,35 @@ Role.create!([
 
 SphinxAdminWordForm.seed
 SphinxAdminException.seed
+
+# Seed default reports
+Report.create(
+  :name        => 'index',
+  :title       => 'Themenübersicht',
+  :orientation => 'portrait',
+  :columns     => [:signature, :title, :document_count],
+  :level       => 2,
+  :per_page    => 'all'
+)
+Report.create(
+  :name        => 'overview',
+  :title       => 'Detailreport',
+  :orientation => 'landscape',
+  :columns     => [:signature, :title, :first_document_year, :container_type, :location, :keyword_text]
+)
+Report.create(
+  :name        => 'year',
+  :title       => 'Jahresreport',
+  :orientation => 'landscape',
+  :columns     => [:signature, :title, :first_document_year],
+  :collect_year_count => 1
+)
+Report.create(
+  :name        => '5-year',
+  :title       => '5-Jahresreport',
+  :orientation => 'portrait',
+  :columns     => [:signature, :title, :document_count],
+  :level       => 2,
+  :per_page    => 'all',
+  :collect_year_count => 5
+)
