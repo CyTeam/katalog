04c899ca9abcfb47e18bffd2b3cb1f8d5379a6d4
Roman Simecek <roman.simecek@cyt.ch>
2011-03-30 10:02:35 +0200

    Search and replace for title realized.

--- app/controllers/search_replaces_controller.rb
+++ app/controllers/search_replaces_controller.rb
@@ -6,8 +6,12 @@ class SearchReplacesController < AuthorizedController
   def create
     @search_replace = SearchReplace.new(params[:search_replace])
     if @search_replace.valid?
-      
+      @search_replace.do
+      flash[:notice] = 'done'
+    else
+      flash[:error] = 'failed'
     end
+
     redirect_to :action => :index
   end
 end
--- app/models/search_replace.rb
+++ app/models/search_replace.rb
@@ -5,7 +5,7 @@ class SearchReplace < FormtasticFauxModel
   validates_presence_of :search, :replace, :columns
   
   def self.editable_attributes
-    [:signature, :description, :title]
+    ['signature', 'description', 'title']
   end
   
   self.types = {
@@ -13,4 +13,17 @@ class SearchReplace < FormtasticFauxModel
     :replace => :string,
     :columns => :string
   }
+  
+  def do
+    columns.each do |column|
+      Dossier.update_all(["#{column} = replace(#{column}, ?, ?)", search, replace], ["#{column} LIKE ?", '%' + search + '%']) if check_column(column)
+    end
+  end
+
+  private
+  def check_column(column)
+    return false if column.empty?
+    
+    SearchReplace.editable_attributes.include?column ? column : nil
+  end
 end
