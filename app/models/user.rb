09775918a9f16a67c3cef4ef0b5be10cc7f79cd7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-05-09 13:15:09 +0200

    Ignore :password_salt and :encrypted_password fields in paper trail for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 # This class holds all the user information's.
 class User < ActiveRecord::Base
-  # PaperTrail: change log
-  has_paper_trail :ignore => [:created_at, :updated_at, :reset_password_token, :remember_token, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip]
+  # PaperTrail: changencrypted_passworde log
+  has_paper_trail :ignore => [
+    :created_at, :updated_at,
+    :password_salt, :encrypted_password,
+    :reset_password_token, :remember_token, :remember_created_at,
+    :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
+  ]
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
