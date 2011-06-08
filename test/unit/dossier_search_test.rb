64852a69ed866b21970025aadfd73c95d6e64ddd
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-06-08 22:51:11 +0200

    Use shoulda context in dossier search test.

--- test/unit/dossier_search_test.rb
+++ test/unit/dossier_search_test.rb
@@ -3,54 +3,56 @@
 require 'test_helper'
 
 class DossierTest < ActiveSupport::TestCase
-  # .split_search_words
-  test "search extraction detects empty search" do
-    assert_equal [[], [], []], Dossier.split_search_words('')
-    assert_equal [[], [], []], Dossier.split_search_words('   ')
-  end
+  context ".split_search_words" do
+    should "detect empty search" do
+      assert_equal [[], [], []], Dossier.split_search_words('')
+      assert_equal [[], [], []], Dossier.split_search_words('   ')
+    end
 
-  test "search extraction detects signatures" do
-    assert_equal [['77.0.'], [], []], Dossier.split_search_words('77.0')
-    assert_equal [['77.0.', '77.0.100', '77.0.10', '7', '77.0.1'], [], []], Dossier.split_search_words('77.0 77.0.100 77.0.10 7 77.0.1')
-  end
+    should "detect signatures" do
+      assert_equal [['77.0.'], [], []], Dossier.split_search_words('77.0')
+      assert_equal [['77.0.', '77.0.100', '77.0.10', '7', '77.0.1'], [], []], Dossier.split_search_words('77.0 77.0.100 77.0.10 7 77.0.1')
+    end
 
-  test "search extraction detects words" do
-    assert_equal [[], ['test', 'new'], []], Dossier.split_search_words('test new')
-    assert_equal [[], ['test', 'new'], []], Dossier.split_search_words('test, new')
-    assert_equal [[], ['test', 'new'], []], Dossier.split_search_words('test. new')
-  end
+    should "detect words" do
+      assert_equal [[], ['test', 'new'], []], Dossier.split_search_words('test new')
+      assert_equal [[], ['test', 'new'], []], Dossier.split_search_words('test, new')
+      assert_equal [[], ['test', 'new'], []], Dossier.split_search_words('test. new')
+    end
 
-  test "search extraction detects signatures and words" do
-    assert_equal [['77.0.'], ['test', 'new'], []], Dossier.split_search_words('test. 77.0, new')
-    assert_equal [['77.0.1', '77.2.'], ['haha', 'bla'], []], Dossier.split_search_words('haha. 77.0.1, bla 77.2')
-  end
+    should "detect signatures and words" do
+      assert_equal [['77.0.'], ['test', 'new'], []], Dossier.split_search_words('test. 77.0, new')
+      assert_equal [['77.0.1', '77.2.'], ['haha', 'bla'], []], Dossier.split_search_words('haha. 77.0.1, bla 77.2')
+    end
 
-  test "search extraction detects year" do
-    assert_equal [[], ['1979'], []], Dossier.split_search_words('1979')
-    assert_equal [[], ['2012'], []], Dossier.split_search_words('2012')
+    should "detect year" do
+      assert_equal [[], ['1979'], []], Dossier.split_search_words('1979')
+      assert_equal [[], ['2012'], []], Dossier.split_search_words('2012')
+    end
+    
+    should "detect double quote sentences" do
+      assert_equal [[], [], ['one']], Dossier.split_search_words('"one"')
+      assert_equal [[], [], ['one two']], Dossier.split_search_words('"one two"')
+      assert_equal [[], ['before', 'between', 'last'], ['one two', 'next, two']], Dossier.split_search_words('before, "one two" between "next, two" last')
+    end
   end
   
-  test "search word extraction detects double quote sentences" do
-    assert_equal [[], [], ['one']], Dossier.split_search_words('"one"')
-    assert_equal [[], [], ['one two']], Dossier.split_search_words('"one two"')
-    assert_equal [[], ['before', 'between', 'last'], ['one two', 'next, two']], Dossier.split_search_words('before, "one two" between "next, two" last')
-  end
-  
-  # .build_query
-  test ".build_query adds * to signature searches" do
-    assert_equal '@signature ("7*")', Dossier.build_query("7")
-    assert_equal '@signature ("77.0.10*")', Dossier.build_query("77.0.10")
-  end
-  
-  test ".build_query adds no * to short search words" do
-    assert_match /[^*]a[^*]/, Dossier.build_query("nr a history")
-  end
-  
-  test ".build_query adds trailing * to medium short search words" do
-    assert_match /[^*]nr\*/, Dossier.build_query("nr a history")
-  end
-  
-  test ".build_query adds surrounding * to non-short search words" do
-    assert_match /\*history\*/, Dossier.build_query("nr as history")
+  context ".build_query" do
+    should "add * to signature searches" do
+      assert_equal '@signature ("7*")', Dossier.build_query("7")
+      assert_equal '@signature ("77.0.10*")', Dossier.build_query("77.0.10")
+    end
+    
+    should "add no * to short search words" do
+      assert_match /[^*]a[^*]/, Dossier.build_query("nr a history")
+    end
+    
+    should "add trailing * to medium short search words" do
+      assert_match /[^*]nr\*/, Dossier.build_query("nr a history")
+    end
+    
+    should "add surrounding * to non-short search words" do
+      assert_match /\*history\*/, Dossier.build_query("nr as history")
+    end
   end
 end
