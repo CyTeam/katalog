b0dacb1c67382c64d19f6538a6c5d7cd4020f498
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-06-09 11:23:25 +0200

    Add functional test for visitor_logs controller.

---
+++ test/functional/visitor_logs_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class VisitorLogsControllerTest  < ActionController::TestCase
+  include Devise::TestHelpers
+  
+  setup do
+    sign_in users(:editor)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+end
