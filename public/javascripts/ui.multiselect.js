269606efe1e590fb20eb3021feff5c9c21582b31
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-06-15 11:31:08 +0200

    Comment out dimension setting in ui.multiselect.js

--- public/javascripts/ui.multiselect.js
+++ public/javascripts/ui.multiselect.js
@@ -57,13 +57,13 @@ $.widget("ui.multiselect", {
 		var that = this;
 
 		// set dimensions
-		this.container.width(this.element.width()+1);
-		this.selectedContainer.width(Math.floor(this.element.width()*this.options.dividerLocation));
-		this.availableContainer.width(Math.floor(this.element.width()*(1-this.options.dividerLocation)));
+//		this.container.width(this.element.width()+1);
+//		this.selectedContainer.width(Math.floor(this.element.width()*this.options.dividerLocation));
+//		this.availableContainer.width(Math.floor(this.element.width()*(1-this.options.dividerLocation)));
 
 		// fix list height to match <option> depending on their individual header's heights
-		this.selectedList.height(Math.max(this.element.height()-this.selectedActions.height(),1));
-		this.availableList.height(Math.max(this.element.height()-this.availableActions.height(),1));
+//		this.selectedList.height(Math.max(this.element.height()-this.selectedActions.height(),1));
+//		this.availableList.height(Math.max(this.element.height()-this.availableActions.height(),1));
 		
 		if ( !this.options.animated ) {
 			this.options.show = 'show';
