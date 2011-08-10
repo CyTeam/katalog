a70766720d070ff09648e6558df4fdee8e161e9e
Roman Simecek <roman.simecek@cyt.ch>
2011-08-10 11:52:33 +0200

    Unused line removed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,7 +40,6 @@ addContainerSuggestionBehaviour();
   end
 
   def show_topic?(topic, search, dossier = nil)
-    # topic.topic_type == :group and ((search and search[:signature] and search[:signature].starts_with?(topic.signature)) || (dossier and dossier.signature.starts_with?(topic.signature))) ? true : false
     (search and search[:signature] and search[:signature].starts_with?(topic.signature)) || (dossier and dossier.signature.starts_with?(topic.signature)) ? true : false
   end
 
