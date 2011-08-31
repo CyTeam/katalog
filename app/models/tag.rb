9a084a27b948047d45c37c49046bb7d80ae192dc
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-31 11:27:33 +0200

    Make tag_cloud dynamic on signature key.

---
+++ app/models/tag.rb
@@ -0,0 +1,22 @@
+# This class is a sub class of ActAsTaggableOn and provides the information about the dossier keywords.
+class Tag < ActsAsTaggableOn::Tag
+
+  # PaperTrail: change log
+  has_paper_trail :ignore => [:created_at, :updated_at]
+
+  # Scopes
+  scope :by_character, lambda {|value| where("name LIKE CONCAT(?, '%')", value)}
+  scope :characters, select("DISTINCT substring(upper(name), 1, 1) AS letter").having("letter BETWEEN 'A' AND 'Z'")
+  default_scope joins(:taggings).where('taggings.context' => 'tags')
+
+  # The list of all characters.
+  def self.character_list
+    characters.map{|t| t.letter}
+  end
+
+  # Dossier scopes
+  scope :by_signature, lambda {|value| where("dossiers.signature LIKE CONCAT(?, '%')", value)}
+
+  scope :filter_tags,
+    select("tags.*, COUNT(*) AS count").joins('JOIN dossiers ON dossiers.id = taggings.taggable_id').having("COUNT(*) >= 2").group("tags.id").order("COUNT(*)").limit(12)
+end
