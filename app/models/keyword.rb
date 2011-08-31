1927b471bdbe5dfc20482b05b3d923a9da030e31
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-31 11:23:21 +0200

    Better performance for Keyword model queries.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -7,7 +7,7 @@ class Keyword < ActsAsTaggableOn::Tag
   # Scopes
   scope :by_character, lambda {|value| where("name LIKE CONCAT(?, '%')", value)}
   scope :characters, select("DISTINCT substring(upper(name), 1, 1) AS letter").having("letter BETWEEN 'A' AND 'Z'")
-  default_scope where("id IN (SELECT tag_id FROM taggings WHERE taggings.context = 'keywords')")
+  default_scope joins(:taggings).where('taggings.context' => 'keywords')
 
   # The list of all characters.
   def self.character_list
