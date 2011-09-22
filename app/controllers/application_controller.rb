71051a20c452e2d6801ab973e9b27e95bff9b6ee
Roman Simecek <roman.simecek@cyt.ch>
2011-09-22 14:02:57 +0200

    Controller logic for setting some display settings to the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  
+  def update_session
+    session[:hide_keywords] = params[:hide_keywords] ? true : false
+    
+    respond_to do |format|
+      format.json { render :json => session }
+    end
+  end
 end
