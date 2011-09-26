ed52d53f136ab7fb0035e69fc5ba7c5754da0ee2
Roman Simecek <roman.simecek@cyt.ch>
2011-09-26 10:11:45 +0200

    Function addEditToolTipBehaviour intends formated.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -141,34 +141,34 @@ function showUnlessNewRecord(container) {
 }
 
 function addEditToolTipBehaviour() {
- $('*[title]').each(function(){
-   if($(this).attr('title')!=''){
-     var target = $(this);
-     if(target.attr('data-parent')) {
-       var haha = target.attr('data-parent');
-       target = $(this).parents(haha);
-       target.attr('title', $(this).attr('title'));
-     }
-     target.qtip({
-                    style: {
-                        name: 'blue',
-                        tip: true,
-                        width: 200,
-                        background: '#E5E8E9',
-                        color: '#00669C',
-                        border: {
-                          color: '#00669C'
-                        }
-                    },
-                    position: {
-                        corner: {
-                             target: 'topRight',
-                             tooltip: 'bottomLeft'
-                        }
-                    }
-             });
-   }
- });
+  $('*[title]').each(function(){
+    if($(this).attr('title')!=''){
+      var target = $(this);
+      if(target.attr('data-parent')) {
+        var haha = target.attr('data-parent');
+        target = $(this).parents(haha);
+        target.attr('title', $(this).attr('title'));
+      }
+      target.qtip({
+        style: {
+          name: 'blue',
+          tip: true,
+          width: 200,
+          background: '#E5E8E9',
+          color: '#00669C',
+          border: {
+            color: '#00669C'
+          }
+        },
+        position: {
+          corner: {
+            target: 'topRight',
+            tooltip: 'bottomLeft'
+          }
+        }
+      });
+    }
+  });
 }
 
 function addSearchSuggestionBehaviour() {
@@ -312,7 +312,7 @@ $(document).ready(function() {
   addAutogrowBehaviour();
   addAutoAddNewContainer();
   addRelationAutoCompletionBehaviour();
-  //addEditToolTipBehaviour();
+  addEditToolTipBehaviour();
   addSearchSuggestionBehaviour();
   addContainerSuggestionBehaviour();
   addUpdateLastContainerTitleOfDossier();
