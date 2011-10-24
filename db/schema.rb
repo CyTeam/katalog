2be22a997b97d58e1d1d3af92c62a8540b283c90
Roman Simecek <roman.simecek@cyt.ch>
2011-10-24 14:41:57 +0200

    DB schema updated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111020133847) do
+ActiveRecord::Schema.define(:version => 20111024121334) do
 
   create_table "container_types", :force => true do |t|
     t.string   "code"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(:version => 20111020133847) do
     t.datetime "updated_at"
     t.string   "title"
   end
+
+  add_index "containers", ["location_id"], :name => "index_containers_on_location_id"
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
