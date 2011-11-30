1d42480d7b3f85a334858f33c966fe6cd5479722
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-30 09:55:15 +0100

    Move raspell to own recipe file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,15 +26,6 @@ require "bundler/capistrano"
 # Clean up the releases after deploy.
 after "deploy", "deploy:cleanup"
 
-# Generate the aspell wordlist
-before 'deploy:restart', 'raspell:generate'
-namespace :raspell do
-  desc 'Generates the aspell wordlist for the suggestions.'
-  task :generate, :roles => :app, :except => { :no_release => true } do
-    run "cd #{release_path} && /usr/bin/env RAILS_ENV=#{rails_env} bundle exec rake katalog:raspell:update"
-  end
-end
-
 # Plugins
 # =======
 # Multistaging
