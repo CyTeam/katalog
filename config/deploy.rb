7e4f1d53b09c60ec9664bca9213dfdbb93282c95
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-30 09:35:58 +0100

    Refactor capistrano deployment to fit best practice.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,12 @@
-require './config/boot'
-require 'airbrake/capistrano'
+require 'capones_recipes/cookbook/rails'
 require 'capones_recipes/tasks/airbrake'
 
 #Application
-set :application, "katalog"
-set :repository,  "git@github.com:CyTeam/katalog.git"
+set :application, 'katalog'
+set :repository,  'git@github.com:CyTeam/katalog.git'
 
 # Staging
-set :stages, %w(production staging fallback)
 set :default_stage, "staging"
-require 'capistrano/ext/multistage'
 
 # Deployment
 set :server, :passenger
@@ -23,18 +20,8 @@ set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :copy_exclude, [".git", "spec"]
 
-# Restart passenger
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
-
 # Bundle install
 require "bundler/capistrano"
-after "bundle:install", "deploy:migrate"
 
 # Clean up the releases after deploy.
 after "deploy", "deploy:cleanup"
@@ -47,3 +34,8 @@ namespace :raspell do
     run "cd #{release_path} && /usr/bin/env RAILS_ENV=#{rails_env} bundle exec rake katalog:raspell:update"
   end
 end
+
+# Plugins
+# =======
+# Multistaging
+require 'capistrano/ext/multistage'
