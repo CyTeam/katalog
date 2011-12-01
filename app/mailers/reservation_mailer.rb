775e29218354aea2ca1b1903ff90c8097965a87a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-01 15:35:16 +0100

    Whitespace fixes in reservation mailer.

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,9 +1,12 @@
 class ReservationMailer < ActionMailer::Base
-  default :from => "info@doku-zug.ch"
+  default :from => 'info@doku-zug.ch'
 
   def user_email(reservation)
     @reservation = reservation
     
-    mail(:to => 'info@doku-zug.ch', :subject => "#{I18n.t('activerecord.models.reservation')}: #{@reservation.dossier}")
+    mail(
+      :to => 'info@doku-zug.ch',
+      :subject => "#{I18n.t('activerecord.models.reservation')}: #{@reservation.dossier}"
+    )
   end
 end
