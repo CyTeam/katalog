3d6159da0459dffcde27c007510d8756d87dd497
Roman Simecek <roman.simecek@cyt.ch>
2012-01-03 14:48:39 +0100

    Remove unused code. Its now used in the dossier model.

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -41,35 +41,7 @@ class VersionsController < AuthorizedController
     if dossier?@version
       Dossier.restore_relations(@version)
     end
-    redirect_to :back
 
-    return
-    
-    version = Version.find(params[:id])
-    object = version.reify
-    if object
-      original = 'destroy'.eql?version.event ? nil : object.class.find(object.id)
-      if original
-        original = object
-        original.save
-      else
-        object.save
-      end
-      object.id_attributes.each do |name, id|
-        sub_version = Version.find_by_item_id(id)
-        sub_object = sub_version.reify
-        sub_original = ('destroy'.eql?sub_version.event ? nil : sub_version.item_type.constantize.find(sub_version.item_id))
-        if sub_original
-          sub_original = sub_object
-          sub_original.save
-        else
-          sub_object.save
-        end
-      end
-    else
-      version.item_type.constantize.find(version.item_id).destroy
-    end
-    
-    redirect_to :action => 'index'
+    redirect_to :back
   end
 end
