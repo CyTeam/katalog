9e310ff78cc41550db9aa52930d43a82f739e09d
Andi Staub (CyT) <andi.staub@cyt.ch>
2012-08-23 12:48:11 +0200

    Made necessary changes to will_paginate to work again with rails3.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,6 +1,4 @@
 # encoding: utf-8
 
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Vorherige Seite'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Nächste Seite'
 WillPaginate::ViewHelpers.pagination_options[:class] = 'digg_pagination'
 WillPaginate::ViewHelpers.pagination_options[:renderer] = 'PaginationListLinkRenderer'
