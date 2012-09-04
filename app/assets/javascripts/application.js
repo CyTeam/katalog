487a6f75430a6c55027768928a90bc28a6bc268d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-04 15:31:25 +0200

    Fix ckeditor and move javascript include_tag to header.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require ckeditor/init
 //= require_tree .
 
 function addContainerSuggestionBehaviour() {
---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,82 @@
+/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+  config.PreserveSessionOnFileBrowser = true;
+  // Define changes to default configuration here. For example:
+  // config.language = 'en';
+  config.language = 'de';
+  // config.uiColor = '#AADC6E';
+
+  //config.ContextMenu = ['Generic','Anchor','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ; 
+  
+  config.height = '400px';
+  config.width = '600px';
+  
+  //config.resize_enabled = false;
+  //config.resize_maxHeight = 2000;
+  //config.resize_maxWidth = 750;
+  
+  //config.startupFocus = true;
+  
+  // works only with en, ru, uk languages
+  // config.extraPlugins = "embed,attachment";
+
+  // Force past as plain text
+  config.ForcePasteAsPlainText = true;
+
+  // Default link target
+  config.DefaultLinkTarget = '_blank' ;
+  
+  config.entities = false;
+  
+  // Link editor customization
+  config.linkShowAdvancedTab = false;
+  config.linkShowTargetTab = false;
+  
+  // Preview customization
+  config.extraPlugins = "embed,attachment,serverpreview";
+  link = window.location.pathname;
+  link_parts = link.split('/');
+  link_parts.pop();
+  link_parts.push('preview');
+  config.serverPreviewURL = window.location.origin + link_parts.join('/')
+  
+  config.toolbar = 'Custom';
+  
+  config.toolbar_Easy =
+    [
+        ['Source','-','Preview','Templates'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord',],
+        ['Maximize','-','About'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Styles','Format'],
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript', 'TextColor'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink','Anchor'],
+        ['Image','Embed','Flash','Attachment','Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
+    ];
+
+  config.toolbar_Small =
+    [
+        ['Source','-','Preview'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord'],
+        ['Maximize','-','About'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript', 'TextColor'],
+        ['NumberedList','BulletedList'],
+        ['Link','Unlink']
+    ];
+
+  config.toolbar_Custom =
+    [
+        ['ServerPreview','-','Cut','Copy','PasteFromWord'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Bold','Italic','Underline','-','Subscript','Superscript','-','BulletedList'],
+        ['Link','Unlink']
+    ];
+};
