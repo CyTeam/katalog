8d25abb93ce5a8c9c9f5772a79fd0c0b010b37e9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-05 13:38:25 +0200

    Push timeout for unicorn to 120s.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ working_directory app_path
 # http://unicorn.bogomips.org/Unicorn/Configurator.html
 preload_app true
 
-timeout 30
+timeout 120
 
 # This is where we specify the socket.
 # We will point the upstream Nginx module to this socket later on
