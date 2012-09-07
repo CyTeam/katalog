8d063bb1c12eaea8d6528b6d6778ec2a9eae686f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-07 12:50:51 +0200

    Inline single-use methods in dossiers controller.

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -86,90 +86,6 @@ class DossiersController < AuthorizedController
   end
 
   def search
-    dossier_search
-  end
-
-
-  # Report Actions
-  # ==============
-  def report
-    report_name = params[:report_name] || 'overview'
-    @report = Report.find_by_name(report_name)
-
-    # Preset parameters
-    case report_name
-      when 'index'
-         @document_count = Dossier.document_count
-    end
-
-    # Sanitize and use columns parameter if present
-    if params[:columns]
-      @report[:columns] = params[:columns].split(',').select{|column| Dossier.columns.include?(column)}
-    end
-
-    # Set pagination parameter
-    params[:per_page] = @report[:per_page] || 'all'
-    @report[:title] ||= report_name
-    @is_a_report = true
-
-    dossier_report
-  end
-
-  def edit_report
-    setup_per_page
-    setup_query
-
-    # Show index if called with no query
-    if @query.blank?
-      @dossiers = Topic.by_level(2)
-      render 'batch_edit_last_year/index' and return
-    end
-
-    # Stay on this action after search
-    @search_path = edit_report_dossiers_path
-
-    # Collection setup
-    @years = DossierNumber.edit_years(params[:dossier_numbers]) if params[:dossier_numbers]
-    @years ||= [Time.now.year - 1]
-
-    if !@signature_search
-      @dossiers = Dossier.by_text(@query, :page => params[:page], :per_page => params[:per_page])
-    else
-      order_attribute = (@query && Topic.alphabetic?(@query)) ? 'signature, title' : 'signature' # Sort with title when query is within an alphabetic topic.
-      @dossiers = Dossier.by_signature(@query).dossier.order(order_attribute).paginate :page => params[:page], :per_page => params[:per_page]
-    end
-  end
-
-  def preview
-    show!
-  end
-
-  # Dangling relates_to list
-  def dangling_relations
-    @dossiers = Dossier.with_dangling_relations
-  end
-
-  def multi_relations
-    @dossiers = Dossier.with_multi_relations
-  end
-
-  private
-  def setup_per_page
-    params[:per_page] ||= 25
-
-    if params[:per_page] == 'all'
-      # Simple hack to simulate all
-      params[:per_page] = 1000000
-    end
-  end
-
-  def setup_query
-    params[:search] ||= {}
-    @query = params[:search][:text].try(:strip) || ''
-    @signature_search = /^[0-9.]{1,8}$/.match(@query)
-  end
-
-  def dossier_search
     setup_per_page
     setup_query
 
@@ -231,7 +147,30 @@ class DossiersController < AuthorizedController
     end
   end
 
-  def dossier_report
+
+  # Report Actions
+  # ==============
+  def report
+    report_name = params[:report_name] || 'overview'
+    @report = Report.find_by_name(report_name)
+
+    # Preset parameters
+    case report_name
+      when 'index'
+         @document_count = Dossier.document_count
+    end
+
+    # Sanitize and use columns parameter if present
+    if params[:columns]
+      @report[:columns] = params[:columns].split(',').select{|column| Dossier.columns.include?(column)}
+    end
+
+    # Set pagination parameter
+    params[:per_page] = @report[:per_page] || 'all'
+    @report[:title] ||= report_name
+    @is_a_report = true
+
+
     setup_per_page
     setup_query
 
@@ -243,6 +182,60 @@ class DossiersController < AuthorizedController
     end
 
     index_excel
+  end
+
+  def edit_report
+    setup_per_page
+    setup_query
+
+    # Show index if called with no query
+    if @query.blank?
+      @dossiers = Topic.by_level(2)
+      render 'batch_edit_last_year/index' and return
+    end
+
+    # Stay on this action after search
+    @search_path = edit_report_dossiers_path
+
+    # Collection setup
+    @years = DossierNumber.edit_years(params[:dossier_numbers]) if params[:dossier_numbers]
+    @years ||= [Time.now.year - 1]
+
+    if !@signature_search
+      @dossiers = Dossier.by_text(@query, :page => params[:page], :per_page => params[:per_page])
+    else
+      order_attribute = (@query && Topic.alphabetic?(@query)) ? 'signature, title' : 'signature' # Sort with title when query is within an alphabetic topic.
+      @dossiers = Dossier.by_signature(@query).dossier.order(order_attribute).paginate :page => params[:page], :per_page => params[:per_page]
+    end
+  end
+
+  def preview
+    show!
+  end
+
+  # Dangling relates_to list
+  def dangling_relations
+    @dossiers = Dossier.with_dangling_relations
+  end
+
+  def multi_relations
+    @dossiers = Dossier.with_multi_relations
+  end
+
+  private
+  def setup_per_page
+    params[:per_page] ||= 25
+
+    if params[:per_page] == 'all'
+      # Simple hack to simulate all
+      params[:per_page] = 1000000
+    end
+  end
+
+  def setup_query
+    params[:search] ||= {}
+    @query = params[:search][:text].try(:strip) || ''
+    @signature_search = /^[0-9.]{1,8}$/.match(@query)
   end
 
   def index_excel
@@ -262,5 +255,4 @@ class DossiersController < AuthorizedController
       }
     end
   end
-
 end
