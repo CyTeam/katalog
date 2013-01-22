bc7304472642ab3934c667739552ee7f9a595d26
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-22 15:16:40 +0100

    Switch rack google analytics gem.

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -3,7 +3,7 @@
 # Google analytics middleware.
 begin
   if Object.const_defined?(:Rack) and Rack.const_defined?(:GoogleAnalytics) and Settings['google_analytics']
-    Rails.application.config.middleware.use("Rack::GoogleAnalytics", :web_property_id => Settings.google_analytics.api_key)
+    Rails.application.config.middleware.use("Rack::GoogleAnalytics", :tracker => Settings.google_analytics.api_key)
   end
 rescue
 end
