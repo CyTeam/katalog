5a548dc77e38f361ddece24d8a76136930339ee3
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-03-01 14:12:26 +0100

    Add dependencies to deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,3 +29,18 @@ set :copy_exclude, [".git", "spec"]
 
 # Clean up the releases after deploy.
 after "deploy", "deploy:cleanup"
+
+# Dependencies
+depend :remote, :gem, 'bundler', '> 0'
+depend :remote, :gem, 'bluepill', ''
+
+# Headers for gem compilation
+depend :remote, :deb, "build-essential", ''
+depend :remote, :deb, "ruby1.9.1-dev", ''
+depend :remote, :deb, "libmysqlclient-dev", ''
+depend :remote, :deb, "libxml2-dev", ''
+depend :remote, :deb, "libxslt1-dev", ''
+depend :remote, :deb, "sphinxsearch", ''
+depend :remote, :deb, "imagemagick", ''
+depend :remote, :deb, "libaspell-dev", ''
+depend :remote, :deb, "aspell-de", ''
