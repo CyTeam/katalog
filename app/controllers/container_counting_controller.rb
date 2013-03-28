0d77c676ea236b0f8396b96d559686a5fd5bf7ae
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-03-28 14:17:34 +0100

    Add signature filter to container_counting form.

--- app/controllers/container_counting_controller.rb
+++ app/controllers/container_counting_controller.rb
@@ -4,9 +4,10 @@ class ContainerCountingController < ApplicationController
   # Filter
   has_scope :by_container_type, :as => :container_type
   has_scope :by_location, :as => :location
+  has_scope :by_signature, :as => :signature
 
   def index
-    if params[:container_type].present? || params[:location].present?
+    if params[:container_type].present? || params[:location].present? || params[:signature].present?
       @dossiers = apply_scopes(Dossier, params).dossier
     else
       @dossiers = nil
