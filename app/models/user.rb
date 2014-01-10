cc98c5b0fe3767be549e7284a1c25c145cb5875e
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-01-10 15:40:00 +0100

    Fix login form after update to newer Rails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     :reset_password_token, :remember_token, :remember_created_at,
     :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
   ]
-  
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable,
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   attr_accessor :login
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
   # Added username and login as accessible attributes for a login with username or email.
   #
