b615d64f5ba8a4d4e06bf4c4969113b8eee777ce
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-01-15 09:25:54 +0100

    Directly link to related dossier if there is an exact match.

--- app/helpers/dossiers_helper.rb
+++ app/helpers/dossiers_helper.rb
@@ -11,7 +11,12 @@ module DossiersHelper
   end
 
   def link_to_relation(relation)
-    link_to(relation, search_dossiers_url(:search => {:text => '"' + relation + '"'}, :skip_topics => 1))
+    dossier = Dossier.where(:title => relation).first
+    if dossier
+      link_to(relation, dossier)
+    else
+      link_to(relation, search_dossiers_url(:search => {:text => '"' + relation + '"'}))
+    end
   end
 
   def availability_text(availability)
