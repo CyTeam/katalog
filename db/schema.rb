a4d82bc898408da7de160eb1056130d16853f7b9
Andi Staub <sta@panter.ch>
2014-11-21 09:55:27 +0100

    Update acts_as_taggable_on.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140110142353) do
+ActiveRecord::Schema.define(:version => 20141121082854) do
 
   create_table "container_types", :force => true do |t|
     t.string   "code"
@@ -153,12 +153,13 @@ ActiveRecord::Schema.define(:version => 20140110142353) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], :name => "taggings_idx", :unique => true
   add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
   add_index "taggings", ["tagger_id", "tagger_type"], :name => "index_taggings_on_tagger_id_and_tagger_type"
 
   create_table "tags", :force => true do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "taggings_count", :default => 0
   end
 
   add_index "tags", ["name"], :name => "index_tags_on_name", :length => {"name"=>10}
