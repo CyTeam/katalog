68e1ecbf2d9f3900ec13fbf972ac9dfff363c0eb
Andi Staub <sta@panter.ch>
2014-12-01 19:40:23 +0100

    Remove unnessecary initializers.

--- config/initializers/google_analytics.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-# Google analytics middleware.
-begin
-  if Object.const_defined?(:Rack) && Rack.const_defined?(:GoogleAnalytics) && Settings['google_analytics']
-    Rails.application.config.middleware.use('Rack::GoogleAnalytics', tracker: Settings.google_analytics.api_key, anonymize_ip: Settings.google_analytics.anonymize_ip)
-  end
-rescue
-end
