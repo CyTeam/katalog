1b34c33ebc07012db1685c870d27750cb630e461
Andi Staub <sta@panter.ch>
2014-12-03 12:29:38 +0100

    Remove containers_controller and views/containers/_new.

--- app/controllers/containers_controller.rb
+++
@@ -1,7 +0,0 @@
-class ContainersController < AuthorizedController
-  # Authentication
-  before_action :authenticate_user!, except: [:index, :search, :show]
-
-  # Responders
-  respond_to :html, :js
-end
